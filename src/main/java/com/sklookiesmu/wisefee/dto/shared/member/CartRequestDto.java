a6bdeb607575dcd899b9fb335a1c0d9267ddff1f
kang1027 <kang3171611@naver.com>
2023-08-09 20:37:50 +0900

    feat : Add function if add Cart, find Cart logic

--- src/main/java/com/sklookiesmu/wisefee/dto/shared/member/CartRequestDto.java
+++ src/main/java/com/sklookiesmu/wisefee/dto/shared/member/CartRequestDto.java
@@ -1,16 +1,42 @@
 package com.sklookiesmu.wisefee.dto.shared.member;
 
+import com.sklookiesmu.wisefee.domain.ProductOptChoice;
 import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-@ApiModel(description = "Cart추가 Dto")
-public class CartRequestDto {
+import java.util.List;
 
-    private Long memberId;
+
+public class CartDto {
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @ApiModel(description = "CartProduct 추가 DTO")
+    public static class CartProductRequestDto {
+        @ApiModelProperty(value = "장바구니 아이디", example = "1")
+        private Long cartId;
+        @ApiModelProperty(value = "카페 아이디", example = "1")
+        private Long cafeId;
+        @ApiModelProperty(value = "상품 아이디", example = "1")
+        private Long productId;
+        @ApiModelProperty(value = "선택상품 옵션 리스트", example = "[1,2,3]")
+        private List<ProductOptionChoiceAsCartRequestDto> productOptChoices;
+
+    }
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @ApiModel(description = "CartProduct 옵션 추가 DTO")
+    public static class ProductOptionChoiceAsCartRequestDto{
+        @ApiModelProperty(value = "선택 상품옵션 아이디", example = "1")
+        private Long optionChoiceId;
+    }
+
 
 }
+
