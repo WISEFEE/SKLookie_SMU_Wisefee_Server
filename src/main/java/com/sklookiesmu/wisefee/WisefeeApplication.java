6aec1d85c5fd802b9cdd58a43a3fff42bf2683ac
Jaehwi-So <sjh9708@gmail.com>
2023-08-10 15:04:03 +0900

    chore : add CORS allow config

--- src/main/java/com/sklookiesmu/wisefee/WisefeeApplication.java
+++ src/main/java/com/sklookiesmu/wisefee/WisefeeApplication.java
@@ -2,12 +2,25 @@ package com.sklookiesmu.wisefee;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class WisefeeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WisefeeApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*");
+			}
+		};
 	}
 
 }
