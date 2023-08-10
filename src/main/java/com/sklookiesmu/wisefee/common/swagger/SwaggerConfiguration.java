09af38a779da56b56764939b36ed969666da294a
soogoori <soobin0209@naver.com>
2023-08-10 15:59:55 +0900

    feat : SwaggerConfiguration Pageable 관련 설정 추가

--- src/main/java/com/sklookiesmu/wisefee/common/swagger/SwaggerConfiguration.java
+++ src/main/java/com/sklookiesmu/wisefee/common/swagger/SwaggerConfiguration.java
@@ -1,10 +1,20 @@
 package com.sklookiesmu.wisefee.common.swagger;
 
+import com.fasterxml.classmate.TypeResolver;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.AlternateTypeRule;
+import springfox.documentation.schema.AlternateTypeRules;
 import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.contexts.SecurityContext;
@@ -15,11 +25,13 @@ import java.util.List;
 
 
 @Configuration
+@RequiredArgsConstructor
 public class SwaggerConfiguration {
-
+    private final TypeResolver typeResolver;
     @Bean
     public Docket api(){
         return new Docket(DocumentationType.OAS_30)
+                .alternateTypeRules(AlternateTypeRules.newRule(typeResolver.resolve(Pageable.class), typeResolver.resolve(Page.class)))
                 .useDefaultResponseMessages(false)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.sklookiesmu.wisefee.api"))
@@ -30,6 +42,16 @@ public class SwaggerConfiguration {
                 .securitySchemes(Arrays.asList(bearerAuthSecurityScheme()));
     }
 
+    @Getter
+    @Setter
+    @ApiModel
+    static class Page{
+        @ApiModelProperty(value = "페이지 번호")
+        private Integer page;
+
+        @ApiModelProperty(value = "페이지 크기")
+        private Integer size;
+    }
     private ApiInfo apiInfo(){
         return new ApiInfoBuilder()
                 .title("Wisefee API Document")
