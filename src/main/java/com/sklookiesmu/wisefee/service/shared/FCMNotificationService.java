171fc07e79523c2720cf7f1497416bd170039787
gomin0 <gomin0981113@naver.com>
2023-08-22 20:16:18 +0900

    feat: 푸시알림 토큰 자동받기

--- src/main/java/com/sklookiesmu/wisefee/service/shared/FCMNotificationService.java
+++ src/main/java/com/sklookiesmu/wisefee/service/shared/FCMNotificationService.java
@@ -1,12 +1,21 @@
 package com.sklookiesmu.wisefee.service.shared;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.FirebaseMessagingException;
 import com.google.firebase.messaging.Message;
 import com.google.firebase.messaging.Notification;
 import com.sklookiesmu.wisefee.dto.shared.firebase.FCMNotificationRequestDto;
 import lombok.RequiredArgsConstructor;
+import okhttp3.*;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.List;
 
 
 @RequiredArgsConstructor
@@ -15,7 +24,20 @@ public class FCMNotificationService {
 
     private final FirebaseMessaging firebaseMessaging;
 
-    public String sendNotificationByToken(FCMNotificationRequestDto requestDto) {
+    private final String API_URL = "https://fcm.googleapis.com/v1/projects/wisefee-b4b12/messages:send";
+
+    private String getAccessToken() throws IOException {
+        String firebaseConfigPath = "firebase/wisefee_service_key.json";
+
+        GoogleCredentials googleCredentials = GoogleCredentials
+                .fromStream(new ClassPathResource(firebaseConfigPath).getInputStream())
+                .createScoped(List.of("https://www.googleapis.com/auth/cloud-platform"));
+
+        googleCredentials.refreshIfExpired();
+        return googleCredentials.getAccessToken().getTokenValue();
+    }
+
+    private String sendNotificationByToken(FCMNotificationRequestDto requestDto) throws JsonProcessingException {
         if (requestDto.getTo() != null) {
             Message message = Message.builder()
                     .setToken(requestDto.getTo())
@@ -36,5 +58,20 @@ public class FCMNotificationService {
         } else {
             return "FCM 토큰 값이 존재하지 않습니다.";
         }
+    }
+
+    public void sendMessageTo(FCMNotificationRequestDto requestDto) throws IOException {
+        String message = sendNotificationByToken(requestDto);
+        OkHttpClient client = new OkHttpClient();
+        RequestBody requestBody = RequestBody.create(message, MediaType.get("application/json; charset=utf-8"));
+        Request request = new Request.Builder()
+                .url(API_URL)
+                .post(requestBody)
+                .addHeader(HttpHeaders.AUTHORIZATION, "Bearer " + getAccessToken())
+                .addHeader(HttpHeaders.CONTENT_TYPE, "application/json; UTF-8")
+                .build();
+        Response response = client.newCall(request)
+                .execute();
+        System.out.println(response.body().string());
     }
 }
